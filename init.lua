ecd302b1e06b34888150d8887aefe6c779498ba7
mightpossibly <111494670+mightpossibly@users.noreply.github.com>
2022-09-03 22:36:00 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@
 local modpath = minetest.get_modpath("upmod")
 dofile(modpath .. "/panes.lua")
 dofile(modpath .. "/nodes.lua")
+
+-- Replace old upmod-sea_lanters with xocean ones
+minetest.register_alias("upmod:sea_lantern", "xocean:sea_lantern")
