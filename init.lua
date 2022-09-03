6270c6ce95c365bb6a0d5c847ba712615fd19183
mightpossibly <111494670+mightpossibly@users.noreply.github.com>
2022-09-03 14:07:00 +0200

    Added no-glitch panes

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Load modfiles
 local modpath = minetest.get_modpath("upmod")
---dofile(modpath .. "/panes.lua")
+dofile(modpath .. "/panes.lua")
 dofile(modpath .. "/nodes.lua")
 --dofile(modpath .. "/sealanterns.lua")
