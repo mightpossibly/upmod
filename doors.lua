70ebe4fc88149a60891a01fb83c36b0bcfd01470
mightpossibly <111494670+mightpossibly@users.noreply.github.com>
2022-12-03 11:57:58 +0100

    added trapdoors

--- doors.lua
+++ doors.lua
@@ -1,7 +1,7 @@
 -- Wood types that already have doors; default:wood and ethereal:sakura_wood
 
 local wood_types = {
-	{"Jungle Tree",	"default:junglewood", 			"jungle",},
+	{"Jungle Tree",	"default:junglewood", 			"junglewood",},
 	{"Pine",		"default:pine_wood", 			"pine",},
 	{"Aspen",		"default:aspen_wood", 			"aspen",},
 	{"Acacia",		"default:acacia_wood", 			"acacia",},
@@ -18,7 +18,7 @@ local wood_types = {
 	{"Willow",		"ethereal:willow_wood", 		"willow",},
 	{"Banana Tree",	"ethereal:banana_wood", 		"banana",},
 	{"Olive Tree",	"ethereal:olive_wood", 			"olive",},
-	{"Redwood",		"ethereal:red_wood", 			"red",},
+	{"Redwood",		"ethereal:red_wood", 			"redwood",},
 	{"Frost Tree",	"ethereal:frost_wood", 			"frost",},
 	{"Healing Tree","ethereal:yellow_wood", 		"yellow",}
 }
@@ -43,5 +43,107 @@ for i in ipairs(wood_types) do
 			{craft, craft}
 		}
 	})
-
+	
+	minetest.register_alias("upmod:door_red", "upmod:door_redwood")
+	minetest.register_alias("upmod:door_red_a", "upmod:door_redwood_a")
+	minetest.register_alias("upmod:door_red_b", "upmod:door_redwood_b")
+	
+	minetest.register_alias("upmod:door_jungle", "upmod:door_junglewood")
+	minetest.register_alias("upmod:door_jungle_a", "upmod:door_junglewood_a")
+	minetest.register_alias("upmod:door_jungle_b", "upmod:door_junglewood_b")
+	
+	doors.register_trapdoor("upmod:trapdoor_"..name, {
+		tiles = {
+			{name = "upmod:trapdoor_"..name..".png", backface_culling = true}
+		},
+		description = (desc.." Trapdoor"),
+	inventory_image = "upmod_trapdoor_"..name..".png",
+	wield_image = "upmod_trapdoor_"..name..".png",
+	tile_front = "upmod_trapdoor_"..name..".png",
+	tile_side = "upmod_trapdoor_"..name.."_side.png",
+	gain_open = 0.06,
+	gain_close = 0.13,
+		groups = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		sound_open = "doors_glass_door_open",
+		sound_close = "doors_glass_door_close",
+	})
 end
+
+
+
+-- Override ethereal:sakura_door
+doors.register(":ethereal:door_sakura", {
+		tiles = {
+			{name = "ethereal_sakura_door.png", backface_culling = true}
+		},
+		description = ("Sakura Wood Door"),
+		inventory_image = "ethereal_sakura_door_inv.png",
+		groups = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		sound_open = "doors_glass_door_open",
+		sound_close = "doors_glass_door_close",
+		recipe = {
+			{"ethereal:sakura_wood", "ethereal:sakura_wood"},
+			{"ethereal:sakura_wood", "ethereal:sakura_wood"},
+			{"ethereal:sakura_wood", "ethereal:sakura_wood"}
+		}
+	})
+
+-- Add Bamboo Door (paper door)
+
+doors.register("upmod:door_bamboo", {
+		tiles = {
+			{name = "door_bamboo.png", backface_culling = true}
+		},
+		description = ("Bamboo Door"),
+		inventory_image = "door_item_bamboo.png",
+		groups = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		sound_open = "doors_glass_door_open",
+		sound_close = "doors_glass_door_close",
+		recipe = {
+			{"group:stick", "default:paper"},
+			{"default:paper", "group:stick"},
+			{"ethereal:bamboo_block", "ethereal:bamboo_block"}
+		}
+	})
+	
+doors.register_trapdoor("upmod:trapdoor_bamboo", {
+		tiles = {
+			{name = "ethereal_paper_wall.png", backface_culling = true}
+		},
+		description = ("Bamboo Trapdoor"),
+	inventory_image = "ethereal_paper_wall.png",
+	wield_image = "ethereal_paper_wall.png",
+	tile_front = "ethereal_paper_wall.png",
+	tile_side = "ethereal_paper_wall.png",
+	gain_open = 0.06,
+	gain_close = 0.13,
+		groups = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 3, flammable = 2},
+		sound_open = "doors_glass_door_open",
+		sound_close = "doors_glass_door_close",
+		recipe = {
+			{"group:stick", "default:paper"},
+			{"default:paper", "group:stick"},
+			{"ethereal:bamboo_block", "group:stick"}
+		}
+	})
+	
+	doors.register_trapdoor("upmod:trapdoor_sakura", {
+		tiles = {
+			{name = "upmod_trapdoor_sakura.png", backface_culling = true}
+		},
+		description = ("Sakura Trapdoor"),
+	inventory_image = "upmod_trapdoor_sakura.png",
+	wield_image = "upmod_trapdoor_sakura.png",
+	tile_front = "upmod_trapdoor_sakura.png",
+	tile_side = "upmod_trapdoor_sakura_side.png",
+	gain_open = 0.06,
+	gain_close = 0.13,
+		groups = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 1, flammable = 2},
+		sound_open = "doors_glass_door_open",
+		sound_close = "doors_glass_door_close",
+		recipe = {
+			{"group:stick", "default:paper"},
+			{"default:paper", "group:stick"},
+			{"ethereal:yellow_wood", "group:stick"}
+		}
+	})
